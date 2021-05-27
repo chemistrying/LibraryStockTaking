49348704d05aa31f3472753bd2208c52f0e0cd33
sy10080 <sy10080@syss.edu.hk>
2021-05-28 00:02:46 +0800

    Investigating beta version

--- library.cpp
+++ library.cpp
@@ -3,17 +3,34 @@
 using namespace std;
 
 int main(){
-    ifstream fin;
-    ofstream fout;
-    fin.open("input.txt");
-	fout.open("output.txt");
-	string c; //format
-	cin >> c;
+	string format;
+	string tracking = "";
+	string buffer[50];
+	int limit = 0;
+	ofstream fout;
+
 	string s; 
-    cin >> s; //fin >> s;
-  
+    getline(cin, s); //fin >> s;
 	while(s != "END"){ //!fin.eof() 
-		fout << c << s << endl;
-        cin >> s; //cin >> s
+		if (s[0] == '['){
+			format = s.substr(1, s.size() - 2);
+		}else if (s[0] == '/'){//command
+			string sa = s.substr(1, 4);
+			if (sa == "SAVE"){
+				string filename = s.substr(6, s.size() - 5);
+				if (filename != "") tracking = filename;
+				fout.open(tracking + ".txt");
+				for (int i = 0; i < limit; i++){
+					fout << buffer[i] << endl;
+				}
+				fout.flush();
+				fout.close();
+			}
+		}else if (limit == 50){
+			cout << "Buffer reached maximum. Please save before entering mroe data." << endl;
+		}else{
+			buffer[limit++] = format + s;
+		}
+		cin >> s;
 	}
 }
