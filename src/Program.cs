8a38c51f919da29e66e866d0b49d4921db6e70ce
sy10080 <sy10080@syss.edu.hk>
2022-05-04 22:43:47 +0800

    Add global variables to Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,12 @@
-﻿public class Program
+﻿public static class Globals
+{
+    public static String _currentFileLocation = "foo";
+    public static Format _format = new Format();
+    public static InputHandler _inputHandler = new InputHandler();
+    public static List<String> Buffer = new List<string>();
+}
+
+public class Program
 {
     static void Main(string[] args)
     {
