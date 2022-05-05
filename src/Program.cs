63cd089bbeafe9eae5d650f91a0f32e486401af1
sy10080 <sy10080@syss.edu.hk>
2022-05-05 22:54:30 +0800

    Update main program

--- src/Program.cs
+++ src/Program.cs
@@ -6,8 +6,10 @@
     public static Config _config = new Config();
     public static InputHandler _inputHandler = new InputHandler();
     public static Commands _commands = new Commands();
+    public static Stack<Tuple<int, List<string>>> _normalUndoStack = new Stack<Tuple<int, List<string>>>();
     public static List<string> _buffer = new List<string>();
     public static List<string> _booklist = new List<string>();
+    public static int[] _originalBooklistIndex = new int[_booklist.Count];
 }
 
 public class Program
@@ -15,6 +17,8 @@ public class Program
     static void Main(string[] args)
     {
         Globals._commands.ReloadConfig();
+        Globals._commands.ReloadBooklist(Globals._config.DefaultBooklistLocation);
+        Globals._commands.LoadSystemMessage();
         do
         {
             string input = Console.ReadLine();
