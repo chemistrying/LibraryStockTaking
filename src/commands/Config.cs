c51329faaf6be27a25bc2ce3fc5af180221e1cd3
sy10080 <sy10080@syss.edu.hk>
2022-05-11 18:01:16 +0800

    Add temp typeof array (commented)

--- src/commands/Config.cs
+++ src/commands/Config.cs
@@ -4,6 +4,7 @@ public class Config
     public string DefaultSaveLocation;
     public bool ShowAllBooksPosition;
     public string[] configs = new string[3] {"DefaultBooklistLocation", "DefaultSaveLocation", "ShowAllBooksPosition"};
+    // public Type[] configTypes = new Type[3] {typeof(string), typeof(string), typeof(bool)};
 
     public Config()
     {
