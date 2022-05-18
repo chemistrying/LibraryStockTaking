c32c36ef2e33bf5024126d47b8a4abb3ce3e1532
sy10080 <sy10080@syss.edu.hk>
2022-05-18 18:44:16 +0800

    Update Config.cs

--- src/Config.cs
+++ src/Config.cs
@@ -2,16 +2,18 @@ public class Config
 {
     public string DefaultBooklistLocation;
     public string DefaultSaveLocation;
+    public string DefaultProgramFilesLocation;
     public bool ShowAllBooksPosition;
     public bool Autocheck;
     public bool BlockInvalidInputs;
-    public string[] configs = new string[5] {"DefaultBooklistLocation", "DefaultSaveLocation", "ShowAllBooksPosition", "Autocheck", "BlockInvalidInputs"};
+    public string[] configs = new string[6] {"DefaultBooklistLocation", "DefaultSaveLocation", "defaultProgramFilesLocation", "ShowAllBooksPosition", "Autocheck", "BlockInvalidInputs"};
     // public Type[] configTypes = new Type[5] {typeof(string), typeof(string), typeof(bool), typeof(bool), typeof(bool)};
 
     public Config()
     {
         DefaultBooklistLocation = "booklist";
         DefaultSaveLocation = "foo";
+        DefaultProgramFilesLocation = "files";
         ShowAllBooksPosition = false;
         Autocheck = false;
         BlockInvalidInputs = false;
