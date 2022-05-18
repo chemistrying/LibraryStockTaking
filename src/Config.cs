f8968606f795629a9dc4d2df2f8419c51bc54eb9
sy10080 <sy10080@syss.edu.hk>
2022-05-18 18:58:14 +0800

    Update Config.cs typo

--- src/Config.cs
+++ src/Config.cs
@@ -6,7 +6,7 @@ public class Config
     public bool ShowAllBooksPosition;
     public bool Autocheck;
     public bool BlockInvalidInputs;
-    public string[] configs = new string[6] {"DefaultBooklistLocation", "DefaultSaveLocation", "defaultProgramFilesLocation", "ShowAllBooksPosition", "Autocheck", "BlockInvalidInputs"};
+    public string[] configs = new string[6] {"DefaultBooklistLocation", "DefaultSaveLocation", "DefaultProgramFilesLocation", "ShowAllBooksPosition", "Autocheck", "BlockInvalidInputs"};
     // public Type[] configTypes = new Type[5] {typeof(string), typeof(string), typeof(bool), typeof(bool), typeof(bool)};
 
     public Config()
