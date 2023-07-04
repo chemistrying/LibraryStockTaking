887d93ff6c217f347cdbc9e579553e363fea8b34
sy10080 <sy10080@syss.edu.hk>
2023-07-04 23:39:03 +0800

    Disable some commands and add parameters

--- src/InputHandler.cs
+++ src/InputHandler.cs
@@ -5,18 +5,18 @@ public class InputHandler
         
     }
 
-    public string HandleInput(string Input)
+    public string HandleInput(string Input, ulong Source, Discord.WebSocket.DiscordSocketClient _client, Discord.WebSocket.SocketUser User)
     {
         if (Input.StartsWith(';'))
         {
             // format changing
-            Globals._format.Change(Input.Length == 1 ? "" : Input.Substring(1));
+            Globals._format[Source].Change(Input.Length == 1 ? "" : Input.Substring(1));
             Serilog.Log.Debug($"Changed input format to {Input}.");
         }
         else if (Input.StartsWith(':'))
         {
             // Ignore format
-            Globals._commands.ReadInput(Input.Substring(1));
+            Globals._commands.ReadInput(Input.Substring(1), Source);
         }
         else if (Input.StartsWith('/'))
         {
@@ -28,19 +28,19 @@ public class InputHandler
                 case "del":
                     // return Globals._commands.Del(args == "" ? 0 : Convert.ToInt32(args));
                     // make it less complicated
-                    return Globals._commands.Del(0);
+                    return Globals._commands.Del(0, Source);
                     break;
-                case "save":
-                    return Globals._commands.Save(args);
-                    break;
+                // case "save":
+                //     return Globals._commands.Save(args);
+                //     break;
                 case "undo":
-                    return Globals._commands.Undo();
+                    return Globals._commands.Undo(Source);
                     break;
                 case "help":
                     return Globals._commands.Help(args);
                     break;
                 case "count":
-                    return Globals._commands.Count(args);
+                    return Globals._commands.Count(Source);
                     break;
                 // case "check":
                 //     return Globals._commands.Check(args);
@@ -63,12 +63,18 @@ public class InputHandler
                 case "search":
                     return Globals._commands.Search(args);
                     break;
+                case "start":
+                    return Globals._commands.Start(args, _client);
+                case "finish":
+                    return Globals._commands.Finish(args, _client, User);
                 default:
                     return "Invalid Command";
                     break;
             }
         }
         
-        return Globals._commands.ReadInput(Globals._format.GetFormat(Input));
+        // TODO: Make formatting better, right now disable it first
+        // return Globals._commands.ReadInput(Globals._format[Source].GetFormat(Input));
+        return Globals._commands.ReadInput(Input, Source);
     }
 }
