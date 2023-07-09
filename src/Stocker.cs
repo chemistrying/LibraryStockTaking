52eb468b897a1ab99a56f5b3120f7aa96422b980
sy10080 <sy10080@syss.edu.hk>
2023-07-10 01:36:12 +0800

    Add bookshelves concept to stocker

--- src/Stocker.cs
+++ src/Stocker.cs
@@ -3,10 +3,12 @@ using Discord.WebSocket;
 public class Stocker
 {
     private SortedSet<string> Container;
+    private Dictionary<ulong, SortedSet<string>> Bookshelves;
 
     public Stocker()
     {
         Container = new SortedSet<string>();
+        Bookshelves = new Dictionary<ulong, SortedSet<string>>();
     }
 
     public void Load(IReadOnlyCollection<SocketTextChannel> channels) 
@@ -18,24 +20,38 @@ public class Stocker
                 string[] Barcodes = File.ReadAllLines($"tempdoc\\{channel.Name}.txt");
                 foreach (string Barcode in Barcodes)
                 {
-                    Add(Barcode);
+                    Add(Barcode, channel.Id);
                 }
             }
         }
     }
 
-    public bool Add(string Barcode)
+    public bool Add(string Barcode, ulong Source)
     {
+        if (!Bookshelves.Keys.Contains(Source)) {
+            Bookshelves.Add(Source, new SortedSet<string>());
+        }
+        Bookshelves[Source].Add(Barcode);
         return Container.Add(Barcode);
     }
 
-    public bool Remove(string Barcode)
+    public bool Remove(string Barcode, ulong Source)
     {
+        Bookshelves[Source].Remove(Barcode);
         return Container.Remove(Barcode);
     }
 
     public bool Duped(string Barcode)
     {
         return Container.Contains(Barcode);
+    }
+
+    public string CheckLocation(string Barcode) {
+        foreach (var Bookshelf in Bookshelves) {
+            if (Bookshelf.Value.Contains(Barcode)) {
+                return Globals._shelfName[Bookshelf.Key];
+            }
+        }
+        return "None";
     }
 }
