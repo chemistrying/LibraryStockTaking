959ee714409309ef3e87f3a9448aac4e1a61ea13
sy10080 <sy10080@syss.edu.hk>
2024-05-27 14:16:12 +0800

    Change API endpoint parameters for single bookshelf query

--- pmlibs-api/Controllers/BookshelvesController.cs
+++ pmlibs-api/Controllers/BookshelvesController.cs
@@ -19,41 +19,26 @@ public class BookshelvesController : ControllerBase
         _bookshelvesService = booksehlvesService;
     }
 
-    [HttpGet("{sessionId:length(24)}/{bookshelfId:length(24)}")]
-    public async Task<ActionResult<Bookshelf>> Get(string sessionId, string bookshelfId)
+    [HttpGet("{bookshelfId:length(24)}")]
+    public async Task<ActionResult<Bookshelf>> Get(string bookshelfId)
     {
-        var session = await _sessionsService.GetAsync(sessionId);
-
-        // check if the session exists and the session is active or not
-        if (session is null)
-        {
-            return NotFound();
-        }
-        else if (!session.IsActive)
-        {
-            return StatusCode(StatusCodes.Status403Forbidden, "The session is currently inactive");
-        }
-
-        // check if the bookshelf is in the session
-        bool bookshelfInSession = false;
-        foreach (var bookshelfGroup in session.AllBookshelfGroups)
-        {
-            if (bookshelfGroup.AllBookshelvesId.Find(x => x.Equals(bookshelfId)) is not null)
-            {
-                bookshelfInSession = true;
-            }
-        }
-
-        // bookshelf id and session id not match (or one of them is wrong)
-        if (!bookshelfInSession)
-        {
-            return NotFound();
-        }
-
         var bookshelf = await _bookshelvesService.GetAsync(bookshelfId);
 
-        // ensure the bookshelf is really there
         if (bookshelf is null)
+        {
+            return NotFound();
+        }
+
+        var sessionId = bookshelf.SessionId;
+
+        // fetch active session
+        var activeSession = await _sessionsService.GetActiveAsync();
+
+        if (activeSession is null)
+        {
+            return NotFound();
+        }
+        else if (activeSession.Id != sessionId)
         {
             return NotFound();
         }
