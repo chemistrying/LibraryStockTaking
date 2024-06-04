4bbc466be099372921737d4c2b0a37e445962984
sy10080 <sy10080@syss.edu.hk>
2024-06-04 13:01:58 +0800

    Disable vue.config.js

--- pmlibs-vue/vue.config.js
+++ pmlibs-vue/vue.config.js
@@ -1,10 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  // devServer: {
-  //     compress: true,
-  //     allowedHosts: [
-  //       'camistryin.ddns.net' // That solved it
-  //     ] 
-  // }
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   devServer: {
+//       compress: true,
+//       allowedHosts: [
+//         'camistryin.ddns.net' // That solved it
+//       ] 
+//   }
+// })
