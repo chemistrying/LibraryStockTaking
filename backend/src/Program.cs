1e2c4154cedaa52499b269e9351cab2b302632db
chemistrying <toto4801110@gmail.com>
2024-11-16 00:06:09 +0800

    Add startup authorization and authentication configuration

--- backend/src/Program.cs
+++ backend/src/Program.cs
@@ -1,6 +1,8 @@
 using Serilog;
+using System.Security.Claims;
 using LibrarySystemApi.Services;
 using LibrarySystemApi.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 LoggerConfiguration loggerConfiguration = new LoggerConfiguration().WriteTo.Console();
 
@@ -24,18 +26,40 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSerilog();
 
 builder.Services.AddControllers();
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton(new SessionsService("sessions"));
 builder.Services.AddSingleton(new BookshelvesService("bookshelves"));
 builder.Services.AddSingleton(new BooksService("books"));
+builder.Services.AddSingleton(new AccountsService("accounts"));
+builder.Services.AddSingleton(new TokensService("tokens"));
 
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 
+builder.Services.AddAuthentication(options => 
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+})
+.AddCookie(options =>
+{
+    options.LoginPath = "/api/Login";
+    options.AccessDeniedPath = "/api";
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy => policy.RequireClaim(ClaimTypes.Role, ["Administrator"]));
+});
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 if (app.Environment.IsDevelopment())
 {
