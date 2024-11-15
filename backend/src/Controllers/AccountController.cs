9fee747c85c5a06683bbfcc59dc83374a1d8a302
chemistrying <toto4801110@gmail.com>
2024-11-16 00:05:28 +0800

    Add authorization-related endpoints

---
+++ backend/src/Controllers/AccountController.cs
@@ -0,0 +1,88 @@
+using LibrarySystemApi.Models;
+using LibrarySystemApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarySystemApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountController : ControllerBase
+{
+    private readonly AccountsService _accountsService;
+    private readonly TokensService _tokensService;
+
+    public AccountController(AccountsService accountsService, TokensService tokensService)
+    {
+        _accountsService = accountsService;
+        _tokensService = tokensService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<string?>> Post(string name, string password)
+    {
+        var account = await _accountsService.GetAsync(name);
+
+        // check if the session exists and the session is active or not
+        if (account is not null)
+        {
+            return BadRequest("The username has already been used.");
+        }
+
+        Account newAccount = new()
+        {
+            Name = name,
+            PasswordHash = PasswordHasher.HashPassword(password, PasswordHasher.GenerateSalt()),
+            IsAdmin = false,
+            CreationTime = DateTime.Now,
+        };
+
+        await _accountsService.CreateAsync(newAccount);
+
+        return name;
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<string?>> Put(AccountPayload accountPayload)
+    {
+        var account = await _accountsService.GetAsync(accountPayload.OldName);
+
+        // check if the session exists and the session is active or not
+        if (account is null)
+        {
+            return BadRequest("The account doesn't exist.");
+        }
+
+        byte[] salt = PasswordHasher.GetSaltFromHash(account.PasswordHash);
+
+        if (await _accountsService.LoginAsync(accountPayload.OldName, PasswordHasher.HashPassword(accountPayload.OldPassword, salt)))
+        {
+            // ok
+            account.Name = accountPayload.NewName ?? accountPayload.OldName;
+            account.PasswordHash = accountPayload.NewPassword == null ? account.PasswordHash : PasswordHasher.HashPassword(accountPayload.NewPassword, PasswordHasher.GenerateSalt());
+            
+            await _accountsService.UpdateAsync(account.Name, account);
+
+            return Ok();
+        }
+
+        return Unauthorized();
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult<string?>> Delete(string name, string password)
+    {
+        var account = await _accountsService.GetAsync(name);
+
+        // check if the account exists
+        if (account is null)
+        {
+            return BadRequest("The account doesn't exist.");
+        }
+
+        byte[] salt = PasswordHasher.GetSaltFromHash(account.PasswordHash);
+
+        await _accountsService.RemoveAsync(name, PasswordHasher.HashPassword(password, salt));
+
+        return name;
+    }
+}
---
+++ backend/src/Controllers/LoginController.cs
@@ -0,0 +1,75 @@
+using LibrarySystemApi.Models;
+using LibrarySystemApi.Services;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace LibrarySystemApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LoginController : ControllerBase
+{
+    private readonly AccountsService _accountsService;
+    private readonly TokensService _tokensService;
+
+    public LoginController(AccountsService accountsService, TokensService tokensService)
+    {
+        _accountsService = accountsService;
+        _tokensService = tokensService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return Unauthorized("You need higher privilege to access the API endpoints.");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(LoginModel loginModel)
+    {
+        string name = loginModel.name, password = loginModel.password;
+
+        var account = await _accountsService.GetAsync(name);
+
+        if (account is null)
+        {
+            return Unauthorized();
+        }
+
+        // Get salt
+        byte[] hashedPasswordWithSalt = Convert.FromBase64String(account.PasswordHash);
+        byte[] salt = new byte[16];
+        Buffer.BlockCopy(hashedPasswordWithSalt, 0, salt, 0, salt.Length);
+
+        bool verdict = await _accountsService.LoginAsync(name, PasswordHasher.HashPassword(password, salt));
+        if (verdict) 
+        {
+            // await _tokensService.RemoveAllAsync(name);
+
+            // Token token = new()
+            // {
+            //     Owner = account.Name,
+            //     ExpiryDate = DateTime.Now.AddDays(1)
+            // };
+
+            // await _tokensService.CreateAsync(token);
+
+            var claims = new List<Claim>
+            {
+                // new Claim(ClaimTypes.NameIdentifier, token.Id!),
+                new Claim(ClaimTypes.Name, name),
+                new Claim(ClaimTypes.Role, account.IsAdmin ? "Administrator" : "User")
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+            
+            return Ok();
+        }
+
+        return Unauthorized();
+    }
+}
